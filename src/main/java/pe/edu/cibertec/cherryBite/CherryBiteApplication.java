a6bef4b62fc9fdb323dceb9109d908ceb3b7aba5
chiszo <gigibethandrea@gmail.com>
2024-06-27 21:46:04 -0500

    creación del proyecto

---
+++ src/main/java/pe/edu/cibertec/cherryBite/CherryBiteApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cherryBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryBiteApplication.class, args);
+	}
+
+}
